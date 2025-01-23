02681e1713bb946f7d7bf807770df7065f9c2bd0
CodeByDanial <70098547+danialisrar09@users.noreply.github.com>
2025-01-23 11:28:36 +0500

    frontend_react

--- frontend_react/src/container/index.js
+++ frontend_react/src/container/index.js
@@ -1,6 +1,6 @@
 export { default as About } from './About/about';
 export { default as Footer } from './Footer/footer';
-export { default as Header } from './Header/header';
-export { default as Skills } from './Skills/skills';
-export { default as About } from './About/about';
-export { default as About } from './About/about';
+export { default as Header } from './Header/Header';
+export { default as Skills } from './Skills/Skills';
+export { default as Testmonials } from './Testimonials/Testimonials';
+export { default as Work } from './Work/Work';
