b98b99f1ea45af435e3d7723f5fed239acb5bad5
CodeByDanial <70098547+danialisrar09@users.noreply.github.com>
2025-02-03 13:06:43 +0500

    frontend_react

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -31,11 +31,13 @@ const Navbar = () => {
             transition={{ duration: 0.85, ease: 'easeOut' }}
           >
             <HiX onClick={() => setToggle(false)} />
-            {['home', 'about', 'contact', 'work', 'skills', 'testinomial'].map((item) => (
-              <li key={item}>
-                <a href={'#${item}'} onClick={() => setToggle(false)}>{item}</a>
-              </li>
-            ))}
+            <ul>
+              {['home', 'about', 'contact', 'work', 'skills', 'testinomial'].map((item) => (
+                <li key={item}>
+                  <a href={'#${item}'} onClick={() => setToggle(false)}>{item}</a>
+                </li>
+              ))}
+            </ul>
           </motion.div>
         )}
       </div>
