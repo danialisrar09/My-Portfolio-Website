f4530b84983f843c2d0e1d36412aefa86d8a24a3
CodeByDanial <70098547+danialisrar09@users.noreply.github.com>
2025-02-13 16:47:06 +0500

    MPW

--- backend_sanity/sanity.config.js
+++ backend_sanity/sanity.config.js
@@ -1,7 +1,7 @@
-import {defineConfig , defineType} from 'sanity'
+import {defineConfig, defineType} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas/index'
+import {schemaTypes} from './schemas/index' 
 
 export default defineConfig({
   name: 'default',
@@ -13,6 +13,6 @@ export default defineConfig({
   plugins: [structureTool(), visionTool()],
 
   schema: {
-  types: schemaTypes.map(type => defineType(type)),
+    types: schemaTypes.map(type => defineType(type)),
   },
 })
